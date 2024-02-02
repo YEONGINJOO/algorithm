cfd8949403d00b7a1b1abaa566e598ae84585d55
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-02-02 09:54:27 +0900

    [Bronze III] Title: J박스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5354. J박스/J박스.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    for i in range(n):
+        if i == 0 or i == n-1:
+            print('#'*n)
+        else:
+            print('#'+'J'*(n-2)+'#')
+    print()
