6e7d645a3970a1dd4e8725b0b81577e49aebb4cb
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-02-18 10:12:37 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.py
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.py
@@ -1,13 +1,13 @@
-N, M, L = map(int, input().split())
+n, m, l = map(int, input().split())
+lst = [0] * n
 cnt = 0
-lst = [0] * N
 lst[0] = 1
 i = 0
-while M not in lst:
-    if lst[i] % 2 == 0:
-        i = (i + L) % N
-    else:
-        i = (i - L) % N
+while m not in lst:
+    if lst[i] % 2 == 1:
+        i = (i + l) % n
+    else:  
+        i = (i - l) % n  
     lst[i] += 1
     cnt += 1
 print(cnt)
