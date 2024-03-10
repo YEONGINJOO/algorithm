6f25c0143af67138858857987d55adb56b7e4728
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-03-10 16:22:40 +0900

    [Bronze II] Title: Number Game, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1975. Number Game/Number Game.py
+++ 백준/Bronze/1975. Number Game/Number Game.py
@@ -1,14 +1,20 @@
 import sys
+
+# 재귀 함수를 사용하여 x를 b로 나누어 떨어지는 횟수를 계산합니다.
+def calculate_zeros(x, b):
+    if x % b == 0:
+        return 1 + calculate_zeros(x // b, b)
+    else:
+        return 0
+
+# 각 숫자에 대한 연속된 0의 합을 미리 계산하여 저장합니다.
+precalculated_zeros = [0] * 1001
+for num in range(2, 1001):
+    precalculated_zeros[num] = sum(calculate_zeros(num, base) for base in range(2, num + 1))
+
+# 입력된 테스트 케이스를 처리합니다.
 input = sys.stdin.readline
-
-def func(x, b):
-    if x !=0 and x % b == 0: return 1+func(x//b,b)
-    else: return 0
-
-c = [0] * 1001
-for x in range(2, 1001):
-    c[x] = sum(func(x, b) for b in range(2, x+1))
-
-for _ in range(int(input())):
+num_of_cases = int(input())
+for _ in range(num_of_cases):
     x = int(input())
-    print(c[x])
+    print(precalculated_zeros[x])
