53c57fe4df76f5e7bb045a3dd1eb4e1fc5d97e16
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-04-01 09:41:03 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 720 ms, Memory: 38300 KB -BaekjoonHub

---
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+lst = [[0] * n for _ in range(n)]
+for i in range(5):
+    for j in range(n):
+        for k in range(j+1, n):
+            if arr[j][i] == arr[k][i]:
+                lst[k][j] = 1
+                lst[j][k] = 1
+cnt = []
+for j in lst:
+    cnt.append(j.count(1))
+print(cnt.index(max(cnt))+1)
