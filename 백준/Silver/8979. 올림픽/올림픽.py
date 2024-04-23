c7bb3cf48655a9f04f927f7775e9dddb2f0fb344
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-04-23 10:33:22 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,14 +1,8 @@
-import sys
-input = sys.stdin.readline
 n, k = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-arr = sorted(arr, key=lambda x: (x[1], x[2], x[3]), reverse=True)
-ans = 0
+lst = [list(map(int, input().split())) for _ in range(n)]
+lst = sorted(lst, key=lambda x: (x[1], x[2], x[3]), reverse=True)
+idx = [lst[i][0] for i in range(n)].index(k)
 for i in range(n):
-    if arr[i][0] == k:
-        if arr[i] == arr[i-1]:
-            ans = i
-        else:
-            ans = i + 1
+    if lst[idx][1:] == lst[i][1:]:
+        print(i+1)
         break
-print(ans)
