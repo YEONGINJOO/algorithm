ad9ead25aaa190b809b6c8b665a009798e7f2a13
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-05-01 11:50:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,11 +4,9 @@ arr = []
 idx = 0
 for i in range(1, n+1):
     idx += k-1
-    if idx < len(lst):
-        arr.append(lst.pop(idx))
-    else:
+    if idx >= len(lst):
         idx %= len(lst)
-        arr.append(lst.pop(idx))
+    arr.append(lst.pop(idx))
 print('<',end='')
 for i in range(n):
     if i < n-1:
