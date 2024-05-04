50764586536edb55d8af9e6c4ed6518808ee2497
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-05-04 10:57:08 +0900

    [Silver IV] Title: 좋은 구간, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+l = int(input())
+lst = list(map(int, input().split()))
+n = int(input())
+lst.sort()
+if n in lst:
+    print(0)
+else:
+    mn = 0
+    mx = 0
+    for i in lst:
+        if i < n:
+            mn = i
+        elif i > n and mx == 0:
+            mx = i
+    mx -= 1
+    mn += 1
+    print((n-mn)*(mx-n+1) + (mx-n))
