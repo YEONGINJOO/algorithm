139ebd9311e8d491b35c5b4c6e03dfc82172a8b8
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-05-17 09:57:35 +0900

    [Silver IV] Title: 주몽, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+my_list = list(map(int, input().split()))
+
+my_list.sort()
+start, end = 0, len(my_list)-1
+cnt = 0 
+
+while start < end: 
+    result = my_list[start] + my_list[end]
+    if result > M:
+        end -= 1 
+    elif result < M:
+        start += 1
+    else:
+        cnt   += 1 
+        start += 1 
+        end   -= 1 
+
+print(cnt)
