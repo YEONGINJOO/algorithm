95bc6334b9b20eece51bc00e8f23124527274eb3
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-06-08 15:53:59 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
@@ -1,9 +1,10 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 if n == 1:
 	n = 4
-else:
+elif n > 1 and n < (10**8) * 5:
 	n = n * 2
+elif n > (10**8) * 5 and n % 2 != 0:
+	n += 1
+elif n > (10**8) * 5 and n % 2 == 0:
+	pass
 print(n)
