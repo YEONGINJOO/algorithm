b6dd0bb6315052b0a8030975d80b7ac27f8959eb
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-06-17 18:15:55 +0900

    [Silver IV] Title: 평행선, Time: 4116 ms, Memory: 40744 KB -BaekjoonHub

---
+++ 백준/Silver/2358. 평행선/평행선.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+# 입력 받기
+n = int(input())
+x_count = defaultdict(int)
+y_count = defaultdict(int)
+
+# 좌표 입력 받기
+for _ in range(n):
+    x, y = map(int, input().split())
+    x_count[x] += 1
+    y_count[y] += 1
+
+# 평행한 직선 개수 세기
+result = 0
+
+# x좌표 기준으로 평행한 직선 개수 세기
+for count in x_count.values():
+    if count > 1:
+        result += 1
+
+# y좌표 기준으로 평행한 직선 개수 세기
+for count in y_count.values():
+    if count > 1:
+        result += 1
+
+# 결과 출력
+print(result)
