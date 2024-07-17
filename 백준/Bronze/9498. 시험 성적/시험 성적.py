afeaf17cc29932c9464f6b86db06e57640cce87f
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-07-17 21:23:04 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+Tscore = int(input())
+if Tscore >= 90:
+    print('A')
+elif Tscore >= 80:
+    print('B')
+elif Tscore >= 70:
+    print('C')
+elif Tscore >= 60:
+    print('D')
+else:
+    print('F')
