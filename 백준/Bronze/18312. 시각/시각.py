4fb2f485818d6cc9a7fbcfe61751597eb0399cb2
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2024-09-05 09:57:39 +0900

    [Bronze II] Title: 시각, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/18312. 시각/시각.py
+++ 백준/Bronze/18312. 시각/시각.py
@@ -1,38 +1,22 @@
 import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
-t = 0
-m = 0
-s = 0
 cnt = 0
-while True:
-    if t == n and m == 59 and s == 59:
-        if len(str(n)) == 1:
-            d = '0' + str(n)
-        else:
-            d = str(n)
-        if str(k) in d or str(k) in str(m) or str(k) in str(s):
-            cnt += 1
-        break
-    if len(str(s)) == 1:
-        a = '0' + str(s)
-    else:
-        a = str(s)
-    if len(str(m)) == 1:
-        b = '0' + str(m)
-    else:
-        b = str(m)
-    if len(str(t)) == 1:
-        c = '0' + str(t)
-    else:
-        c = str(t)
-    if str(k) in a or str(k) in b or str(k) in c:
-        cnt += 1
-    s += 1
-    if s > 59:
-        s = 0
-        m += 1
-        if m > 59:
-            m = 0
-            t += 1
+for hour in range(n+1):
+    for minute in range(60):
+        for second in range(60):
+            if len(str(hour)) == 1:
+                a = '0' + str(hour)
+            else:
+                a = str(hour)
+            if len(str(minute)) == 1:
+                b = '0' + str(minute)
+            else:
+                b = str(minute)
+            if len(str(second)) == 1:
+                c = '0' + str(second)
+            else:
+                c = str(second)
+            if str(k) in a + b + c:
+                cnt += 1
 print(cnt)
