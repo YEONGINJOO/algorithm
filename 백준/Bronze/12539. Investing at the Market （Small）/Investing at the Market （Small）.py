028520fbc6d79309666ab4c0705f9ae4fdda331b
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2025-02-14 17:41:48 +0900

    [Bronze I] Title: Investing at the Market (Small), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/12539. Investing at the Market （Small）/Investing at the Market （Small）.py
+++ 백준/Bronze/12539. Investing at the Market （Small）/Investing at the Market （Small）.py
@@ -10,8 +10,8 @@ for k in range(1, N + 1):
     best_buy_month = -1
     best_sell_month = -1
 
-    for buy in range(11):  # 구매는 1~11월 (0~10 인덱스)
-        for sell in range(buy + 1, 12):  # 판매는 구매 다음 달부터 12월까지
+    for buy in range(11):
+        for sell in range(buy + 1, 12):
 
             buy_price = lst[buy]
             sell_price = lst[sell]
