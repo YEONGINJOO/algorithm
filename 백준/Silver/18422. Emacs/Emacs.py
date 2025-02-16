69523c4e89fb93b74eb869454f504969ec72e0c6
YeongIn Joo <71751287+YEONGINJOO@users.noreply.github.com>
2025-02-16 17:41:28 +0900

    [Silver IV] Title: Emacs, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18422. Emacs/Emacs.py
+++ 백준/Silver/18422. Emacs/Emacs.py
@@ -1,34 +1,21 @@
-def count_rectangles(N, M, grid):
-    visited = [[False] * M for _ in range(N)]
-    rectangle_count = 0
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = [input().strip() for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+cnt = 0
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == '*' and not visited[i][j]:
+            cnt += 1
+            x, y = i, j
+            while y < M and arr[i][y] == '*':
+                y += 1
 
-    for i in range(N):
-        for j in range(M):
-            if grid[i][j] == '*' and not visited[i][j]:
-                # 새로운 직사각형 발견
-                rectangle_count += 1
-                
-                # 직사각형의 경계를 찾기
-                x_end, y_end = i, j
-                
-                # 오른쪽 경계 찾기
-                while y_end < M and grid[i][y_end] == '*':
-                    y_end += 1
-                
-                # 아래쪽 경계 찾기
-                while x_end < N and all(grid[x_end][y] == '*' for y in range(j, y_end)):
-                    x_end += 1
-                
-                # 방문 처리
-                for x in range(i, x_end):
-                    for y in range(j, y_end):
-                        visited[x][y] = True
+            while x < N and all(arr[x][k] == '*' for k in range(j, y)):
+                x += 1
 
-    return rectangle_count
-
-N, M = map(int, input().strip().split())
-grid = [input().strip() for _ in range(N)]
-
-# 직사각형 개수 세기
-result = count_rectangles(N, M, grid)
-print(result)
+            for n in range(i, x):
+                for m in range(j, y):
+                    visited[n][m] = True
+print(cnt)
